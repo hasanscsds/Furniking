789d4fb32ebaefaf8a0f9691ed1b752e1d1b3779
KhasanDev <63578294+hasanscsds@users.noreply.github.com>
2022-07-26 23:26:44 +0500

    b

--- js/main.js
+++ js/main.js
@@ -1,80 +1,78 @@
 
 //PRELOADER
-   $(window).on('load', function () {
-   $(".loader").fadeOut();
-   $("#preloder").delay(400).fadeOut("slow")
-   var mixer = mixitup('.filtered__products__trending, filtered__our__products');
-   var mixer = mixitup(containerEl);
-   var mixer = mixitup(containerEl, {
-      selectors: {
-         target: '.blog-item'
-      },
-      animation: {
-         duration: 300
-      }
-   });
-   
+$(window).on('load', function() {
+	$(".loader").fadeOut();
+	$("#preloder").delay(400).fadeOut("slow")
+	var mixer = mixitup('.filtered__products__trending, filtered__our__products');
+	var mixer = mixitup(containerEl);
+	var mixer = mixitup(containerEl, {
+		selectors: {
+			target: '.blog-item'
+		},
+		animation: {
+			duration: 300
+		}
+	});
+
 });
-
-
 
 
 //OWL CAROUSEL
 $('.customer__comments__block .container .owl-carousel').owlCarousel({
-   loop:true,
-   nav:true,
-   dots: false,
-   responsiveClass:true,
-   autoplay: true,
-   autoplaySpeed: 2000,
-   navSpeed: 2000,
-   lazyContent: true,
-   autoplayHoverPause: true,
-   mouseDrag: true,
-   touchDrag: true,
-   responsive:{
-      0:{
-         items:1,
-      },
-      600:{
-         items:1,
-      },
-      1000:{
-         items:1,
-      }
-   }
+	loop: true,
+	nav: true,
+	dots: false,
+	responsiveClass: true,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	navSpeed: 2000,
+	lazyContent: true,
+	autoplayHoverPause: true,
+	mouseDrag: true,
+	touchDrag: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		600: {
+			items: 1,
+		},
+		1000: {
+			items: 1,
+		}
+	}
 })
 
 $('.banner__block .container  .owl-carousel').owlCarousel({
-   loop:true,
-   dots: true,
-   responsiveClass:true,
-   dotsSpeed: 2000,
-   dotsEach: true,
-   lazyContent: true,
-   autoplayHoverPause: true,
-   mouseDrag: true,
-   touchDrag: true,
-   responsive:{
-      0:{
-         items:1,
-      },
-      600:{
-         items:1,
-      },
-      1000:{
-         items:1,
-      }
-   }
+	loop: true,
+	dots: true,
+	responsiveClass: true,
+	dotsSpeed: 2000,
+	dotsEach: true,
+	lazyContent: true,
+	autoplayHoverPause: true,
+	mouseDrag: true,
+	touchDrag: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		600: {
+			items: 1,
+		},
+		1000: {
+			items: 1,
+		}
+	}
 })
 $(document).ready(function() {
-   $(".banner__collections__mobile__btn").click(function(){
-      $(".banner__collections").toggleClass("active").toggle(500)
-   })
-   $(".icon_close").click(function(){
-      $(".banner__collections").toggleClass("active").toggle(500)
-   })
-   $(".banner__collections__sidebar__nav .nav__item").click(function(){
-      $(".banner__collections").toggleClass("active").toggle(500)
-   })
+	$(".banner__collections__mobile__btn").click(function() {
+		$(".banner__collections").toggleClass("active").toggle(500)
+	})
+	$(".icon_close").click(function() {
+		$(".banner__collections").toggleClass("active").toggle(500)
+	})
+	$(".banner__collections__sidebar__nav .nav__item").click(function() {
+		$(".banner__collections").toggleClass("active").toggle(500)
+	})
 });
